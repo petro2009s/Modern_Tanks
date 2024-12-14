936241e52f617e77d4bd2f91540446f123535863
SurgeonMPT <47532294+SurgeonMPT@users.noreply.github.com>
2024-12-14 16:36:15 +0300

    update by Artem

--- bin/settings.py
+++ bin/settings.py
@@ -4,17 +4,17 @@ WIDTH = 1920
 HEIGHT = 1080
 
 button_color = (50, 60, 50)
-
-icon = pygame.image.load('resources/images/icon_test2_p.png')
-
-menu1 = pygame.image.load('resources/images/menu_p1.png')
-menu2 = pygame.image.load('resources/images/menu_p2.png')
-menu3 = pygame.image.load('resources/images/menu_p3.png')
-menu4 = pygame.image.load('resources/images/menu_p4.png')
-menu5 = pygame.image.load('resources/images/menu_p5.png')
-menu_list = [menu1, menu2, menu3, menu4, menu5]
+pygame.init()
 
 display = pygame.display.set_mode((WIDTH, HEIGHT))
+icon = pygame.image.load('resources/images/icon_test2_p.png').convert()
+
+menu1 = pygame.image.load('resources/images/menu_p1.png').convert()
+menu2 = pygame.image.load('resources/images/menu_p2.png').convert()
+menu3 = pygame.image.load('resources/images/menu_p3.png').convert()
+menu4 = pygame.image.load('resources/images/menu_p4.png').convert()
+menu5 = pygame.image.load('resources/images/menu_p5.png').convert()
+menu_list = [menu1, menu2, menu3, menu4, menu5]
 
 graph_dict = {0: False, 1: True, 2: False}
 d_dict = {0: False, 1: True, 2: False}
@@ -25,7 +25,9 @@ lvl_dict = {0: True}
 clock = pygame.time.Clock()
 FPS = 60
 
-with open('resources/descriptions/he.txt', encoding='utf-8') as f, open('resources/descriptions/apfsds.txt', encoding='utf-8') as f2, open('resources/descriptions/heat.txt', encoding='utf-8') as f3:
+with (open('resources/descriptions/he.txt', encoding='utf-8') as f,
+      open('resources/descriptions/apfsds.txt', encoding='utf-8') as f2,
+      open('resources/descriptions/heat.txt', encoding='utf-8') as f3):
     HE = list(map(lambda x: x[:-1], f.readlines()))
     APFSDS = list(map(lambda x: x[:-1], f2.readlines()))
     HEAT = list(map(lambda x: x[:-1], f3.readlines()))
@@ -35,4 +37,10 @@ with open('resources/descriptions/T-90M_descr.txt', encoding='utf-8') as f:
     T_90M_descr = list(map(lambda x: x[:-1], f.readlines()))
 with open('resources/descriptions/T-90M_TTH.txt', encoding='utf-8') as f:
     T_90M_TTH = list(map(lambda x: x[:-1], f.readlines()))
+with open('resources/descriptions/guide.txt', encoding='utf-8') as f:
+    guide_descr = list(map(lambda x: x[:-1], f.readlines()))
 
+MAX_AMMO = 22
+APFSDS_COUNT = 0
+HE_COUNT = 0
+HEAT_COUNT = 0
