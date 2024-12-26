76fc1a30494cc893d8951624285ed435b11fa491
Rayan <abud.rayan09@gmail.com>
2024-12-26 21:54:23 +0300

    collisions update2

--- bin/tank.py
+++ bin/tank.py
@@ -4,13 +4,17 @@ import math
 from bin.text import Text
 from bin.test_settings import TankSettings
 
+
 class Tank:
     def __init__(self, settings, x, y, movement_angle):
         self.s = settings
         self.x = x
         self.y = y
         self.v = 0
-        self.side = int(self.s.WIDTH * 0.015)
+        print(self.s.map.world_map, sep='\n')
+        self.stuck = False
+        self.side = int(self.s.WIDTH * 0.016)
+        print(self.side)
         self.tank_rect = pygame.Rect(x, y, self.side, self.side)
         self.movement_angle = movement_angle
 
@@ -40,7 +44,8 @@ class Tank:
             fps_count_text.set_another_text(str(int(self.s.clock.get_fps())) + ' FPS')
             fps_count_text_bl.draw(self.s.display)
             fps_count_text.draw(self.s.display)
-
+            pygame.draw.rect(self.s.display, (50, 50, 50),
+                             (self.tank_rect.x, self.tank_rect.y, self.tank_rect.width, self.tank_rect.height))
             pygame.display.flip()
             self.s.clock.tick(self.s.FPS)
 
@@ -70,12 +75,15 @@ class Tank:
         dx = s * sin_a
         dy = s * cos_a
 
-        # self.check_collisions(dx, dy)
+        # self.check(dx, dy)
         if dx != 0 or dy != 0:
             self.collisions(dx, dy)
+            if self.stuck:
+                v = 0
         else:
             self.y -= dy
             self.x += dx
+        # v = 0 if self.stuck else v
         self.v = v
 
         if self.v > self.s.min_speed_ad or self.v < 0:
@@ -88,12 +96,14 @@ class Tank:
                 self.v = self.v * 0.95
 
     def check_collisions(self, dx, dy):
+        self.stuck = False
         next_rect = self.tank_rect.copy()
         next_rect.move_ip(dx, dy)
         hits = next_rect.collidelistall(self.s.map.collision_walls)
+        delta_x = 0
+        delta_y = 0
         if len(hits) > 0:
-            delta_x = 0
-            delta_y = 0
+            print(dx, dy)
             for i in hits:
                 hit = self.s.map.collision_walls[i]
                 if dx > 0:
@@ -101,53 +111,98 @@ class Tank:
                 else:
                     delta_x += hit.right - next_rect.left
                 if dy > 0:
-                    delta_y += next_rect.bottom - hit.top
+                    delta_y -= next_rect.bottom - hit.top
                 else:
+
                     delta_y += hit.bottom - next_rect.top
-            print(delta_x, delta_y)
+            # print(delta_y)
             if abs(delta_x - delta_y) < 10:
                 dx = 0
                 dy = 0
-            elif delta_y > delta_x:
-                dx = 0
             elif delta_x > delta_y:
+                dy = 0
+                print(1, delta_x, delta_y)
+            # self.stuck = True
+            elif delta_y > delta_x:
+                print(2, delta_x, delta_y)
+                dx = 0
+
+                # self.stuck = True
+        self.x += dx
+        self.y -= dy
+
+    def check(self, dx, dy):
+        self.stuck = False
+        print()
+        if dx != 0:
+            delta_x = (self.side // 2) * abs(dx) / dx
+            if self.check_wall((self.x + dx + delta_x) // self.s.tile_w, (self.y + delta_x) // self.s.tile_h):
+                dx = 0
+                print(1.5)
+                self.stuck = True
+            if self.check_wall((self.x + dx + delta_x) // self.s.tile_w, (self.y - delta_x) // self.s.tile_h):
+                dx = 0
+                print(2.5)
+                self.stuck = True
+        if dy != 0:
+            delta_y = (self.side // 2) * abs(dy) / dy
+            print((self.y - delta_y + dy), self.y)
+            if self.check_wall((self.x + delta_y) // self.s.tile_w, (self.y - delta_y + dy) // self.s.tile_h):
+                self.stuck = True
+                print(1)
+                dy = 0
+            if self.check_wall((self.x - delta_y) // self.s.tile_w, (self.y + dy - delta_y) // self.s.tile_h):
+                print(2)
                 dy = 0
         self.x += dx
         self.y -= dy
 
     def check_wall(self, x, y):
+        return (int(x), int(y)) in self.s.map.world_map
 
-        return (x, y) in self.s.map.world_map
     def collisions(self, dx, dy):
-        print(0.5, self.x, self.y, dx, dy)
-        # if not self.check_wall((self.x + dx) // self.s.tile_w, (self.y + dy) // self.s.tile_h):
-        #     self.y -= dy
-        #     self.x += dx
-        #     print(1, self.x, self.y)
-        if self.check_wall(self.x // self.s.tile_w, self.y // self.s.tile_h):
-            print(2)
-            self.y += dy
-            self.x -= dx
-        elif not self.check_wall(self.x // self.s.tile_w, self.y // self.s.tile_h):
-            self.y -= dy
-            self.x += dx
-            print(2.5)
-        elif self.check_wall((self.x + dx) // self.s.tile_w, self.y // self.s.tile_h):
-            self.y -= dy
+        self.stuck = False
+        ty = 0
+        if dy != 0:
+            ty = self.side // 2 * abs(dy) / dy
+        tx = 0
+        if dx != 0:
+            tx = self.side // 2 * abs(dx) / dx
+
+        if self.check_wall((self.x + tx + dx) // self.s.tile_w, (self.y - ty) // self.s.tile_h):
+            print(((self.x + tx + dx) // self.s.tile_w + 1) * self.s.tile_w)
+            if tx > 0:
+                temp = ((self.x + tx + dx) // self.s.tile_w) * self.s.tile_w
+            else:
+                temp = ((self.x + tx + dx) // self.s.tile_w + 1) * self.s.tile_w
+            self.x += temp - self.x - tx
+            self.stuck = True
             print(3)
 
-        elif self.check_wall(self.x // self.s.tile_w, (self.y - dy) // self.s.tile_h):
+        if self.check_wall((self.x + tx) // self.s.tile_w, (self.y - dy - ty) // self.s.tile_h):
             print(4)
+            if ty > 0:
+                temp = ((self.y - dy - ty) // self.s.tile_h + 1) * self.s.tile_h
+                print(((self.y - dy - ty) // self.s.tile_h) * self.s.tile_h)
+            else:
+                print(((self.y - dy - ty) // self.s.tile_h) * self.s.tile_h)
+                temp = ((self.y - dy - ty) // self.s.tile_h) * self.s.tile_h
             print(dy)
+            print(-temp + self.y - ty)
+
+            self.y -= -temp + self.y - ty
+
+            self.stuck = True
+        if not self.check_wall((self.x + tx + dx) // self.s.tile_w, (self.y - ty - dy) // self.s.tile_h):
+            self.y -= dy
             self.x += dx
-
-
-
+            self.stuck = False
+           # print(2.5, ((self.x + tx), (self.y - ty)))
 
     def pos(self):
         return (self.x, self.y)
 
 
-# s = TankSettings()
-# ex = Tank(s, 150, 150, 0)
-# ex.start()
+s = TankSettings()
+ex = Tank(s, 150, 150, 0)
+ex.start()
